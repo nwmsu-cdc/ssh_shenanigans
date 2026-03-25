8c5441d7e834bf06315d2b40975b630e4cad1bf8
APnda <apnda6661@proton.me>
2026-03-25 18:21:28 -0500

    allow tools to be seen by host

--- caddy_site/run_tools.sh
+++ caddy_site/run_tools.sh
@@ -5,4 +5,5 @@ run echo "PS1='\[\e[92m\]\u\[\e[0m\]@\[\e[94m\]\h\[\e[0m\]:\[\e[35m\]\w\[\e[0m\]
 cmd ["/bin/bash"]
 EOF
 ) -t ssh_tools; 
-podman run -it -e TERM=xterm-256color ssh_tools
+# podman network create ssh_tools_net --subnet 10.88.0.0/24 --gateway 10.88.0.1;
+podman run -it --network podman -P --ip=10.88.0.8 -e TERM=xterm-256color ssh_tools;
